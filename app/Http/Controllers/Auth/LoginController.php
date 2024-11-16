c25c94134dfbf3d219f64da62bddeaa45abcfb12
Bruno Huang <brunohuang2012@gmail.com>
2024-11-16 18:36:37 +0000

    redirect to home after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,7 +41,7 @@ class LoginController extends Controller
             if (Auth::guard($guard)->attempt($credentials, $request->filled('remember'))) {
                 $request->session()->regenerate();
                 
-                return redirect()->intended('/helloworld');
+                return redirect()->intended('/home');
             }
         }
 
