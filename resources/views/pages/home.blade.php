e44ffd90699d902d06714e94d9cfe66a75a1757c
Daniel Basílio <dbasilio15@gmail.com>
2024-11-17 14:06:48 +0000

    added cart button in home page for testing

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,6 +6,7 @@
     <section id="home">
         <article>
             <h1>Hello World</h1>
+            <button onclick="window.location.href = '{{ route('shopping_cart') }}';">Shopping Cart Button</button>
             <p>This is a simple example page.</p>
             <p><strong>IN CONTRUCTION...</strong></p>
             <p><strong>IN CONTRUCTION...</strong></p>
