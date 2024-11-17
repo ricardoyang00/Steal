fe06127a32b5da788eeb0c869136221459d3bc39
Ricardo Yang <ricardo.yang00@gmail.com>
2024-11-17 14:40:57 +0000

    conflict solve

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,11 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
+    use HasFactory;
+
     protected $table = 'game';
 
     public $timestamps  = false;
 
-    protected $fillable = ['name', 'price'];
+    protected $fillable = [
+        'name',
+        'description',
+        'minimum_age',
+        'price',
+        'overall_rating',
+        'owner',
+        'is_active',
+        'release_date'
+    ];
+
+    public function seller()
+    {
+        return $this->belongsTo(Seller::class, 'owner', 'id');
+    }
 
     public function shoppingCarts()
     {
