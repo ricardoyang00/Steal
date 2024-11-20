055b2a81f6c017e939dda36375706652eca3ab9a
Bruno Huang <brunohuang2012@gmail.com>
2024-11-20 20:34:33 +0000

    adjusted game detail page image size

--- resources/views/pages/game-details.blade.php
+++ resources/views/pages/game-details.blade.php
@@ -3,20 +3,18 @@
 @section('title', $game->name)
 
 @section('content')
-<div class="container py-5">
-    <div class="row">
-        <div class="col-md-6">
-            <img src="{{ asset('images/default-game-image.jpg') }}" class="img-fluid" alt="{{ $game->name }}">
-        </div>
-        <div class="col-md-6">
-            <h1>{{ $game->name }}</h1>
-            <p><strong>Description:</strong> {{ $game->description }}</p>
-            <p><strong>Owner:</strong> {{ $game->seller->name }}</p>
-            <p><strong>Minimum Age:</strong> {{ $game->minimum_age }}</p>
-            <p><strong>Price:</strong> ${{ $game->price }}</p>
-            <p><strong>Rating:</strong> {{ $game->overall_rating }}%</p>
-            <a href="#" class="btn btn-primary">Buy Now</a>
-        </div>
+<div class="game-details-page">
+    <div class="game-image">
+        <img src="{{ asset('images/default-game-image.jpg') }}" class="img-fluid" alt="{{ $game->name }}">
+    </div>
+    <div class="game-details">
+        <h1>{{ $game->name }}</h1>
+        <p><strong>Description:</strong> {{ $game->description }}</p>
+        <p><strong>Owner:</strong> {{ $game->seller->name }}</p>
+        <p><strong>Minimum Age:</strong> {{ $game->minimum_age }}</p>
+        <p><strong>Price:</strong> ${{ $game->price }}</p>
+        <p><strong>Rating:</strong> {{ $game->overall_rating }}%</p>
+        <a href="#" class="btn btn-primary">Buy Now</a>
     </div>
 </div>
 
