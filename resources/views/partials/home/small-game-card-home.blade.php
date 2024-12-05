04859e1e19f009e8b49612bee58b1e97c1f0359a
Bruno Huang <brunohuang2012@gmail.com>
2024-12-05 18:53:34 +0000

    minor improves in home screen

--- resources/views/partials/home/small-game-card-home.blade.php
+++ resources/views/partials/home/small-game-card-home.blade.php
@@ -8,5 +8,9 @@
         <p class="game-price">
             €{{ number_format($game->price, 2) }}
         </p>
+        <!-- Game Title -->
+        <a class="game-title" href="{{ route('game.details', ['id' => $game->id]) }}">
+            {{ $game->name }}
+        </a>
     </div>
 </div>
