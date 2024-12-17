b916e147574d1f08241a78fd934bb53ae9fb81d3
Ricardo Yang <ricardo.yang00@gmail.com>
2024-12-17 09:19:16 +0000

    purchase details styling

--- resources/views/pages/purchase-details.blade.php
+++ resources/views/pages/purchase-details.blade.php
@@ -3,26 +3,29 @@
 @section('title', 'Purchase Details')
 
 @section('content')
-<div class="container mt-5">
+<div class="puchase-details-page">
     <h1>Purchase Details</h1>
 
     <div class="purchase-details">
-        <p><strong>Game:</strong> {{ $game->name }}</p>
-        <p><strong>CDK:</strong> {{ $purchase->cdk_code }}</p>
-        <p><strong>Buyer Name:</strong> {{ $purchase->buyer_name }}</p>
-        <p><strong>Buyer Email:</strong> {{ $purchase->buyer_email }}</p>
-        <p><strong>Value:</strong> €{{ number_format($purchase->value, 2) }}</p>
-        <p><strong>Purchase Date:</strong> {{ \Carbon\Carbon::parse($purchase->time)->format('d/m/Y H:i:s') }}</p>
-        <p><strong>Payment Method:</strong>
-            {{ $purchase->payment_method_name }}
-            <img src="{{ asset($purchase->payment_method_image) }}" alt={{ $purchase->payment_method_name }} style="width:1.54em;">
+        <p><strong>Game</strong> <a href="{{ route('game.details', ['id' => $game->id]) }}">{{ $game->name }}</a></p>
+        <p><strong>CDK</strong> {{ $purchase->cdk_code }}</p>
+        <p><strong>Buyer Name</strong> {{ $purchase->buyer_name }}</p>
+        <p><strong>Buyer Email</strong> {{ $purchase->buyer_email }}</p>
+        <p><strong>Value</strong> €{{ number_format($purchase->value, 2) }}</p>
+        <p><strong>Purchase Date</strong> {{ \Carbon\Carbon::parse($purchase->time)->format('d/m/Y H:i:s') }}</p>
+        <p>
+            <strong>Payment Method</strong>
+            <span class="payment-method">
+                {{ $purchase->payment_method_name }}
+                <img src="{{ asset($purchase->payment_method_image) }}" alt="{{ $purchase->payment_method_name }}">
+            </span>
         </p>
         @if(is_admin())
-            <p><strong>Seller Name:</strong> {{ $seller->seller_name }}</p>
-            <p><strong>Seller Email:</strong> {{ $seller->seller_email }}</p>
+            <p><strong>Seller Name</strong> {{ $seller->seller_name }}</p>
+            <p><strong>Seller Email</strong> {{ $seller->seller_email }}</p>
         @endif
     </div>
 
-    <a href="{{ url()->previous() }}" class="btn btn-primary">Back</a>
+    <a href="{{ url()->previous() }}" class="back-btn">Back</a>
 </div>
 @endsection
