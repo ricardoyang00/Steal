1ab1809c7965d2d9e43c6ab9b5936856ae320c71
Francisco Magalhães <up202007945@up.pt>
2024-12-18 19:17:36 +0000

    Finishing order receipt page css.

--- resources/views/checkout/orderCompleted.blade.php
+++ resources/views/checkout/orderCompleted.blade.php
@@ -9,56 +9,89 @@
     <p class="lead">Thank you for your purchase! Here is your receipt:</p>
 
     <table class="receipt-main-table">
-        <thead>
-            <tr>
-                <th colspan="2">Receipt Details</th>
-            </tr>
-        </thead>
         <tbody>
-            <!-- Ordered Games Section -->
+            <!-- Ordered Games Subtable -->
             @if (count($prePurchasedItems) > 0)
-                <tr class="purchase-type-header">
-                    <td colspan="2">Ordered Games</td>
+                <tr>
+                    <td>
+                        <div class="subtable-container">
+                            <table class="subtable ordered-games">
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Ordered Games</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($prePurchasedItems as $item)
+                                        <tr class="purchase-items">
+                                            <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
+                                            <td>€{{ number_format($item['value'], 2) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
-                @foreach ($prePurchasedItems as $item)
-                    <tr class="purchase-items">
-                        <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
-                        <td>€{{ number_format($item['value'], 2) }}</td>
-                    </tr>
-                @endforeach
             @endif
 
-            <!-- Delivered Games Section -->
+            <!-- Delivered Games Subtable -->
             @if (count($purchasedItems) > 0)
-                <tr class="purchase-type-header">
-                    <td colspan="2">Delivered Games</td>
+                <tr>
+                    <td>
+                        <div class="subtable-container">
+                            <table class="subtable delivered-games">
+                                <thead>
+                                    <tr>
+                                        <th colspan="3">Delivered Games</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($purchasedItems as $item)
+                                        <tr class="purchase-items">
+                                            <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
+                                            <td>{{ $item['cdkCode'] ?? 'Unknown' }}</td>
+                                            <td>€{{ number_format($item['value'], 2) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
-                @foreach ($purchasedItems as $item)
-                    <tr class="purchase-items">
-                        <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
-                        <td>{{ $item['cdkCode'] ?? 'Unknown' }}</td>
-                        <td>€{{ number_format($item['value'], 2) }}</td>
-                    </tr>
-                @endforeach
             @endif
 
-            <!-- Canceled Purchases Section -->
+            <!-- Canceled Purchases Subtable -->
             @if (count($canceledItems) > 0)
-                <tr class="purchase-type-header">
-                    <td colspan="2">Canceled Purchases</td>
+                <tr>
+                    <td>
+                        <div class="subtable-container">
+                            <table class="subtable canceled-purchases">
+                                <thead>
+                                    <tr>
+                                        <th colspan="2">Canceled Purchases</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($canceledItems as $item)
+                                        <tr class="purchase-items">
+                                            <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
+                                            <td>€{{ number_format($item['value'], 2) }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </td>
                 </tr>
-                @foreach ($canceledItems as $item)
-                    <tr class="purchase-items">
-                        <td>{{ $item['gameName'] ?? 'Unknown' }}</td>
-                        <td>€{{ number_format($item['value'], 2) }}</td>
-                    </tr>
-                @endforeach
             @endif
         </tbody>
         <tfoot>
             <tr class="receipt-footer">
-                <td class="footer-left">Coins Used: {{ $coinsUsed }}</td>
-                <td class="footer-right">Total Price: €{{ number_format($subtotal, 2) }}</td>
+                <td colspan="3">
+                    <div class="footer-left">Coins Used: {{ $coinsUsed }}</div>
+                    <div class="footer-right">Total Price: €{{ number_format($subtotal, 2) }}</div>
+                </td>
             </tr>
         </tfoot>
     </table>
@@ -69,6 +102,3 @@
     </div>
 </div>
 @endsection
-
-
-
